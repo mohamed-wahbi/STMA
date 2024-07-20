43e27ab6a8fe3809eeca89fd0fd9717fefdd0556
mohamed-wahbi <mohamedwahbimohamedwahbi@gmail.com>
2024-07-20 11:38:35 +0200

    creation d'une base des données STMA et connecter avec elle  .

---
+++ Server/Config/connect.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/STMA')
+.then(()=>console.log('Connecting with DB *_*'))
+.catch((error)=>console.log(error))
+
+module.exports=mongoose;
