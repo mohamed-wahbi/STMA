c69f1f924c5c2afc10830d15115b5734e0c803e4
mohamed-wahbi <mohamedwahbimohamedwahbi@gmail.com>
2024-07-26 10:44:40 +0200

    creation d'un middlwear pour verifier l'object id si il et correct ou pas pour maximiser la securisation des donner et de navigation avec un systeme de response d'error claire .

---
+++ Server/middlweare/verifyObjectId.js
@@ -0,0 +1,7 @@
+const mongoose = require ('mongoose');
+module.exports = (req,res,next)=>{
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        res.status(400).json({message:'Invalid ID'})
+    }
+    next();
+}
